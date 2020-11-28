171fb8e167eacbef503f628ba673548465b165a5
AVIPAGHADAR1729 <avipaghadar2001@gmail.com>
2020-11-28 13:19:56 +0530

    new Change

--- app.js
+++ app.js
@@ -69,14 +69,27 @@ const FileFilter = function (req, file, cb) {
 
 };
 
+
+const pdfFilter = function(req,file,cb){
+       var ext = path.extname(file.originalname);
+       if(ext!=='.pdf'){
+        return cb("Please Select the pdf File Only..")
+      }
+      cb(null,true)
+}
+
 var uploadImg = multer({ storage: storage, fileFilter: imageFilter });
 var uploadFile = multer({ storage: storage, fileFilter: FileFilter });
+var pdfUpload = multer({ storage: storage, fileFilter: pdfFilter })
 
 
 app.get('/uploadImg',(req,res)=>{
     res.sendFile(__dirname+"/index.html")
 })
 
+app.get('/pdfUpload',(req,res)=>{
+    res.sendFile(__dirname+"/pdfs_merge_tool.html")
+})
 
 
 app.get('/',function(req,res){
@@ -85,7 +98,7 @@ app.get('/',function(req,res){
 
 /// #### upload and process Image ####
 
-app.post('/processimage',uploadImg.single('file'),function(req,res){
+/*app.post('/processimage',uploadImg.single('file'),function(req,res){
   
      format = req.body.format;
      width = parseInt(req.body.width);
@@ -110,8 +123,67 @@ app.post('/processimage',uploadImg.single('file'),function(req,res){
      }
 
 
-});
+});*/
 
+var outputFilePath;
+var ratio;
+
+app.post('/processimage',uploadImg.single('file'),function(req,res){
+  
+     format = req.body.format;
+     width = parseInt(req.body.width);
+     height = parseInt(req.body.height);
+     ratio = req.body.ratio;
+     console.log(ratio)
+
+     if(ratio=='yes'){
+          
+
+          if(req.file){
+            console.log(req.file.path);
+
+            if(isNaN(height)){
+              var dimensions = imageSize(req.file.path);
+              console.log(dimensions)
+              width = parseInt(req.body.width);
+              var rt = (dimensions.width/dimensions.height);
+              height = Math.ceil(width/rt);
+
+              
+
+              console.log(rt+' '+width+' '+height)
+              processImage(width,height,req,res)
+              
+
+             }
+
+   }
+}
+     else{
+
+
+      if(req.file){
+      console.log(req.file.path);
+      
+      if(isNaN(width) || isNaN(height)){
+        var dimensions = imageSize(req.file.path);
+        console.log(dimensions)
+        width = parseInt(dimensions.width);
+        height = parseInt(dimensions.height);
+        processImage(width,height,req,res)
+      }
+      else{
+                processImage(width,height,req,res)
+      }
+
+
+     }
+
+     }
+     
+
+
+});
 
 
 
@@ -131,7 +203,7 @@ app.post('/docxprocess',uploadFile.single('file'),function(req,res){
 
   const selected_file = fs.readFileSync(req.file.path);
   //console.log(selected_file);
-  outputFile=(Math.random()*9999+1)+"outputFile.pdf";
+  outputFile=Math.ceil(Math.random()*9999+1)+"outputPDFFile.pdf";
 
   toPdf(selected_file).then((pdfBuffer) => {
 
@@ -160,6 +232,78 @@ app.post('/docxprocess',uploadFile.single('file'),function(req,res){
 
 
 
+
+
+// ############ PDF PRocessing ##################
+
+const pdf_merge = require('easy-pdf-merge')
+
+var outputFinalFile;
+app.post('/processPdf',pdfUpload.array('file',20),(req,res)=>{
+
+  const files = []
+  outputFinalFile = Math.ceil(Math.random()*9999+1)+'-output.pdf';
+
+
+  if(req.files){
+
+       req.files.forEach(file=>{
+          console.log(file.path)
+          files.push(file.path)
+       })
+
+
+  pdf_merge(files,outputFinalFile,(err)=>{
+         if(err){
+            res.send(" Please Select the File then Press Download..")
+         }
+         res.download(outputFinalFile,(err)=>{
+            if(err){
+              res.send("error in Mergeing File in download try again later...")
+            }
+            fs.unlinkSync(outputFinalFile)
+            
+
+            return Promise.all(files.map(file=>{
+                 new Promise((res,rej)=>{
+
+                   try{
+                      fs.unlink(file,(err)=>{
+                          if(err) throw err;
+                          console.log("Successfully Deleted")
+                      })
+                   }
+                   catch(err){
+
+                   }
+                 })
+            })
+            );
+
+
+
+          ///#####################
+         })
+         
+  })
+      
+    
+
+  }
+
+
+  
+})
+
+
+
+
+
+
+
+
+//  ##########################
+
 app.listen(PORT,function(){
 	console.log(`App is Start at ${PORT}`)
 });
@@ -167,7 +311,7 @@ app.listen(PORT,function(){
 var outputFilePath;
 function processImage(width,height,req,res){
 
-   outputFilePath = Date() + "output." + format;
+   outputFilePath = Date.now() + "output." + format;
   if (req.file) {
     sharp(req.file.path)
       .resize(width, height)
